228812dd485bd5239a7576f738ad3dd3ccef87f8
ccli8 <ccli8@nuvoton.com>
2017-04-25 18:51:17 +0800

    Add more serial tests

--- main.cpp
+++ main.cpp
@@ -89,16 +89,22 @@
 
 #endif
 
+static void test_serial_tx_attach(void);
+static void test_serial_rx_attach(void);
+static void test_serial_txrx_attach(void);
+static void serial_tx_callback(Serial *serial);
+static void serial_rx_callback(Serial *serial);
 static void test_serial_tx_async(void);
 static void test_serial_rx_async(void);
-static void test_serial_async_callback(int event);
+static void test_serial_tx_async_n_tx_attach(void);
+static void serial_async_callback(int event);
 static void test_spi_master(void);
 static void test_spi_master_async(void);
-static void test_spi_master_async_callback(int event);
+static void spi_master_async_callback(int event);
 static void test_spi_slave(void);
 static void test_i2c_master(void);
 static void test_i2c_master_async(void);
-static void test_i2c_master_async_callback(int event);
+static void i2c_master_async_callback(int event);
 static void test_i2c_slave(void);
 static void test_interruptin(void);
 static void my_gpio_irq_rise(void);
@@ -110,13 +116,13 @@ static DigitalOut led3(LED_BLUE, 1);
 
 static union {
     struct {
-        char i2c_buf[32];
-        char i2c_buf2[32];
-    };
+        char buf[32];
+        char buf2[32];
+    } i2c_test_ctx;
     struct {
-        MYCONF_TRAN_UNIT_T spi_buf[32];
-        MYCONF_TRAN_UNIT_T spi_buf2[32];
-    };
+        MYCONF_TRAN_UNIT_T buf[32];
+        MYCONF_TRAN_UNIT_T buf2[32];
+    } spi_test_ctx;
 };
 
 volatile int callback_event = 0;
@@ -128,10 +134,14 @@ int MY_BUF_POS = 0;
 
 int main()
 {
-    //test_serial_tx_async();
+    //test_serial_tx_attach();
+    //test_serial_rx_attach();
+    //test_serial_txrx_attach();
+    test_serial_tx_async();
     //test_serial_rx_async();
+    //test_serial_tx_async_n_tx_attach();
     //test_spi_master();
-    test_spi_master_async();
+    //test_spi_master_async();
     //test_spi_slave();
     //test_i2c_master();
     //test_i2c_master_async();
@@ -143,19 +153,70 @@ static char serial_buf_tx[] = "1234567800000000001111111111222222222233333333334
 //static char serial_buf_tx[23] = "12345678901234567890\n\n";
 //static char serial_buf_tx[11] = "123456789\n";
 static char serial_buf_rx[23];
-static Serial my_serial(SERIAL_TX, SERIAL_RX);
 static volatile int my_serial_event = 0;
+
+static void test_serial_tx_attach(void)
+{
+    static Serial my_serial(SERIAL_TX, SERIAL_RX);
+    
+    Callback<void()> callback(&serial_tx_callback, &my_serial);
+    my_serial.attach(callback, mbed::Serial::TxIrq);
+    
+    while (1) {
+        my_serial.putc('.');
+        wait(1.0);
+    }
+}
+
+static void test_serial_rx_attach(void)
+{
+    static Serial my_serial(SERIAL_TX, SERIAL_RX);
+    
+    Callback<void()> callback(&serial_rx_callback, &my_serial);
+    my_serial.attach(callback, mbed::Serial::RxIrq);
+    
+    while (1) {
+        wait(1.0);
+    }
+}
+
+static void test_serial_txrx_attach(void)
+{
+    static Serial my_serial(SERIAL_TX, SERIAL_RX);
+    
+    Callback<void()> tx_callback(&serial_tx_callback, &my_serial);
+    Callback<void()> rx_callback(&serial_rx_callback, &my_serial);  
+    my_serial.attach(tx_callback, mbed::Serial::TxIrq);
+    my_serial.attach(rx_callback, mbed::Serial::RxIrq);
+    
+    while (1) {
+        wait(1.0);
+    }
+}
+
+static void serial_tx_callback(Serial *serial)
+{
+    led2 = ! led2;
+}
+
+static void serial_rx_callback(Serial *serial)
+{
+    led3 = ! led3;
+    // NOTE: On Nuvoton targets, no H/W IRQ to match RxIrq. Simulation of RxIrq requires the call to Serial::getc(). 
+    serial->putc(serial->getc());
+}
 
 static void test_serial_tx_async(void)
 {   
-    event_callback_t event_callback(test_serial_async_callback);
+    static Serial my_serial(SERIAL_TX, SERIAL_RX);
+    event_callback_t event_callback(serial_async_callback);
     
 REPEAT:
     my_serial_event = 0;
-    my_serial.set_dma_usage_tx(DMA_USAGE_NEVER);
-    //my_serial.set_dma_usage_tx(DMA_USAGE_ALWAYS);
+    //my_serial.set_dma_usage_tx(DMA_USAGE_NEVER);
+    my_serial.set_dma_usage_tx(DMA_USAGE_ALWAYS);
     
-    my_serial.set_flow_control(SerialBase::RTSCTS, SERIAL_RTS, SERIAL_CTS);
+    //my_serial.set_flow_control(SerialBase::RTSCTS, SERIAL_RTS, SERIAL_CTS);
     
     my_serial.write((const uint8_t *) serial_buf_tx, sizeof (serial_buf_tx) - 1, event_callback, SERIAL_EVENT_TX_COMPLETE);
     
@@ -171,15 +232,16 @@ REPEAT:
 
 static void test_serial_rx_async(void)
 {   
-    event_callback_t event_callback(test_serial_async_callback);
+    static Serial my_serial(SERIAL_TX, SERIAL_RX);
+    event_callback_t event_callback(serial_async_callback);
     
 REPEAT:
     my_serial_event = 0;
     memset(serial_buf_rx, 0x00, sizeof (serial_buf_rx));
-    my_serial.set_dma_usage_tx(DMA_USAGE_NEVER);
-    //my_serial.set_dma_usage_rx(DMA_USAGE_ALWAYS);
+    //my_serial.set_dma_usage_tx(DMA_USAGE_NEVER);
+    my_serial.set_dma_usage_rx(DMA_USAGE_ALWAYS);
     
-    my_serial.set_flow_control(SerialBase::RTSCTS, SERIAL_RTS, SERIAL_CTS);
+    //my_serial.set_flow_control(SerialBase::RTSCTS, SERIAL_RTS, SERIAL_CTS);
     
     //my_serial.read((uint8_t *) serial_buf_rx, sizeof (serial_buf_rx) - 1, event_callback, SERIAL_EVENT_RX_COMPLETE, SERIAL_RESERVED_CHAR_MATCH);    
     my_serial.read((uint8_t *) serial_buf_rx, sizeof (serial_buf_rx) - 1, event_callback, SERIAL_EVENT_RX_COMPLETE | SERIAL_EVENT_RX_CHARACTER_MATCH, 'q');
@@ -211,7 +273,39 @@ REPEAT:
     }
 }
 
-void test_serial_async_callback(int event)
+void test_serial_tx_async_n_tx_attach(void)
+{
+    static Serial my_serial(SERIAL_TX, SERIAL_RX);
+    
+    Callback<void()> callback(&serial_tx_callback, &my_serial);
+    my_serial.attach(callback, mbed::Serial::TxIrq);
+    
+    event_callback_t event_callback(serial_async_callback);
+    
+REPEAT:
+    wait(1.0);
+    
+    my_serial.putc('.');
+    // Wait for UART TX FIFO empty
+    wait_ms(100);
+    
+    my_serial_event = 0;
+    my_serial.set_dma_usage_tx(DMA_USAGE_NEVER);
+    //my_serial.set_dma_usage_tx(DMA_USAGE_ALWAYS);
+    
+    my_serial.write((const uint8_t *) serial_buf_tx, sizeof (serial_buf_tx) - 1, event_callback, SERIAL_EVENT_TX_COMPLETE);
+    
+    while (! my_serial_event) {
+        wait_ms(200);
+    }
+    
+    if (my_serial_event & SERIAL_EVENT_TX_COMPLETE) {
+        my_serial.printf("SERIAL_EVENT_TX_COMPLETE event received\n");
+        goto REPEAT;
+    }
+}
+
+void serial_async_callback(int event)
 {
     my_serial_event = event;
 }
@@ -287,8 +381,8 @@ static void test_spi_master_async(void)
     spi_master.set_dma_usage(MYCONF_DMA_USAGE);
     spi_master.format(MYCONF_BITS_TRAN_UNIT_T);    // n bits per SPI frame
     // Fill in transmit buffer
-    for (int i = 0; i < sizeof (spi_buf) / sizeof (spi_buf[0]); i ++) {
-        spi_buf[i] = i;
+    for (int i = 0; i < sizeof (spi_test_ctx.buf) / sizeof (spi_test_ctx.buf[0]); i ++) {
+        spi_test_ctx.buf[i] = i;
     }
     
     // NOTE: Run spi_master first and then run spi_slave 3 secs. This is to keep cs inactive until spi_slave is ready.
@@ -300,7 +394,7 @@ static void test_spi_master_async(void)
 REPEAT:
 
     // Clear receive buffer
-    memset(spi_buf2, 0xFF, sizeof (spi_buf2));
+    memset(spi_test_ctx.buf2, 0xFF, sizeof (spi_test_ctx.buf2));
     
 #if (! MYCONF_SPI_AUTOSS)
     cs = 0;
@@ -310,8 +404,8 @@ REPEAT:
 #endif
 
     callback_event = 0;
-    event_callback_t callback(test_spi_master_async_callback);
-    if (spi_master.transfer(spi_buf, sizeof (spi_buf) / sizeof (spi_buf[0]), spi_buf2, sizeof (spi_buf2) / sizeof (spi_buf2[0]), callback, SPI_EVENT_ALL)) {
+    event_callback_t callback(spi_master_async_callback);
+    if (spi_master.transfer(spi_test_ctx.buf, sizeof (spi_test_ctx.buf) / sizeof (spi_test_ctx.buf[0]), spi_test_ctx.buf2, sizeof (spi_test_ctx.buf2) / sizeof (spi_test_ctx.buf2[0]), callback, SPI_EVENT_ALL)) {
         printf("%s FAILED\n", __func__);
 #if (! MYCONF_SPI_AUTOSS)
         cs = 1;
@@ -339,7 +433,7 @@ REPEAT:
     }
     if (callback_event & SPI_EVENT_COMPLETE) {
         printf("SPI_EVENT_COMPLETE\n");
-        if (memcmp(spi_buf + MYCONF_SPI_ECHO_PLUS, spi_buf2 + 2, sizeof (spi_buf) - sizeof (MYCONF_TRAN_UNIT_T) * MYCONF_SPI_ECHO_PLUS)) {
+        if (memcmp(spi_test_ctx.buf + MYCONF_SPI_ECHO_PLUS, spi_test_ctx.buf2 + 2, sizeof (spi_test_ctx.buf) - sizeof (MYCONF_TRAN_UNIT_T) * MYCONF_SPI_ECHO_PLUS)) {
             printf("%s Round %d FAILED\n", __func__, n_round ++);
             while (1);
         }
@@ -350,7 +444,7 @@ REPEAT:
     }
 }
 
-static void test_spi_master_async_callback(int event)
+static void spi_master_async_callback(int event)
 {
     callback_event = event;
 }
@@ -391,42 +485,42 @@ static void test_i2c_master(void)
     int n_round = 0;
     
     // Fill in transmit buffer
-    for (int i = 0; i < sizeof (i2c_buf) / sizeof (i2c_buf[0]); i ++) {
-        i2c_buf[i] = i;
+    for (int i = 0; i < sizeof (i2c_test_ctx.buf) / sizeof (i2c_test_ctx.buf[0]); i ++) {
+        i2c_test_ctx.buf[i] = i;
     }
 
 REPEAT:
-    i2c_master.write(MYCONF_I2C_ADDR, i2c_buf, sizeof (i2c_buf), 1);
-    memset(i2c_buf2, 0xFF, sizeof (i2c_buf2));
-    i2c_master.read(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2), 1);
+    i2c_master.write(MYCONF_I2C_ADDR, i2c_test_ctx.buf, sizeof (i2c_test_ctx.buf), 1);
+    memset(i2c_test_ctx.buf2, 0xFF, sizeof (i2c_test_ctx.buf2));
+    i2c_master.read(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2), 1);
     
-    i2c_master.write(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2), 1);
-    i2c_master.write(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2), 1);
-    i2c_master.write(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2), 1);
-    memset(i2c_buf2, 0xFF, sizeof (i2c_buf2));
-    i2c_master.read(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2), 1);
+    i2c_master.write(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2), 1);
+    i2c_master.write(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2), 1);
+    i2c_master.write(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2), 1);
+    memset(i2c_test_ctx.buf2, 0xFF, sizeof (i2c_test_ctx.buf2));
+    i2c_master.read(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2), 1);
     
-    i2c_master.write(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2));
-    memset(i2c_buf2, 0xFF, sizeof (i2c_buf2));
-    i2c_master.read(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2));
+    i2c_master.write(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2));
+    memset(i2c_test_ctx.buf2, 0xFF, sizeof (i2c_test_ctx.buf2));
+    i2c_master.read(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2));
     
-    i2c_master.write(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2));
-    i2c_master.write(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2));
-    i2c_master.write(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2));
-    memset(i2c_buf2, 0xFF, sizeof (i2c_buf2));
-    i2c_master.read(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2));
+    i2c_master.write(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2));
+    i2c_master.write(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2));
+    i2c_master.write(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2));
+    memset(i2c_test_ctx.buf2, 0xFF, sizeof (i2c_test_ctx.buf2));
+    i2c_master.read(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2));
     
-    i2c_master.write(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2));
-    memset(i2c_buf2, 0xFF, sizeof (i2c_buf2));
-    i2c_master.read(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2));
-    memset(i2c_buf2, 0xFF, sizeof (i2c_buf2));
-    i2c_master.read(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2));
-    memset(i2c_buf2, 0xFF, sizeof (i2c_buf2));
-    i2c_master.read(MYCONF_I2C_ADDR, i2c_buf2, sizeof (i2c_buf2));
+    i2c_master.write(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2));
+    memset(i2c_test_ctx.buf2, 0xFF, sizeof (i2c_test_ctx.buf2));
+    i2c_master.read(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2));
+    memset(i2c_test_ctx.buf2, 0xFF, sizeof (i2c_test_ctx.buf2));
+    i2c_master.read(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2));
+    memset(i2c_test_ctx.buf2, 0xFF, sizeof (i2c_test_ctx.buf2));
+    i2c_master.read(MYCONF_I2C_ADDR, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2));
     
-    char *buf_pos = i2c_buf;
-    char *buf_end = i2c_buf + sizeof (i2c_buf) / sizeof (i2c_buf[0]);
-    char *buf2_pos = i2c_buf2;
+    char *buf_pos = i2c_test_ctx.buf;
+    char *buf_end = i2c_test_ctx.buf + sizeof (i2c_test_ctx.buf) / sizeof (i2c_test_ctx.buf[0]);
+    char *buf2_pos = i2c_test_ctx.buf2;
     int data;
     int data2;
     while (buf_pos < buf_end) {
@@ -449,17 +543,17 @@ static void test_i2c_master_async(void)
     int n_round = 0;
 
     // Fill in transmit buffer
-    for (int i = 0; i < sizeof (i2c_buf) / sizeof (i2c_buf[0]); i ++) {
-        i2c_buf[i] = i;
+    for (int i = 0; i < sizeof (i2c_test_ctx.buf) / sizeof (i2c_test_ctx.buf[0]); i ++) {
+        i2c_test_ctx.buf[i] = i;
     }
     
 REPEAT:
     // Clear receive buffer
-    memset(i2c_buf2, 0xFF, sizeof (i2c_buf2));
+    memset(i2c_test_ctx.buf2, 0xFF, sizeof (i2c_test_ctx.buf2));
     
     callback_event = 0;
-    event_callback_t callback(test_i2c_master_async_callback);
-    if (i2c_master.transfer(MYCONF_I2C_ADDR, i2c_buf, sizeof (i2c_buf) / sizeof (i2c_buf[0]), i2c_buf2, sizeof (i2c_buf2) / sizeof (i2c_buf2[0]), callback, I2C_EVENT_ALL, 0)) {
+    event_callback_t callback(i2c_master_async_callback);
+    if (i2c_master.transfer(MYCONF_I2C_ADDR, i2c_test_ctx.buf, sizeof (i2c_test_ctx.buf) / sizeof (i2c_test_ctx.buf[0]), i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf2) / sizeof (i2c_test_ctx.buf2[0]), callback, I2C_EVENT_ALL, 0)) {
         printf("%s FAILED\n", __func__);
         while (1);
     }
@@ -481,7 +575,7 @@ REPEAT:
     }
     if (callback_event & I2C_EVENT_TRANSFER_COMPLETE) {
         printf("I2C_EVENT_TRANSFER_COMPLETE\n");
-        if (memcmp(i2c_buf + 1, i2c_buf2, sizeof (i2c_buf) - sizeof (char) * 1)) {
+        if (memcmp(i2c_test_ctx.buf + 1, i2c_test_ctx.buf2, sizeof (i2c_test_ctx.buf) - sizeof (char) * 1)) {
             printf("%s Round %d FAILED\n", __func__, n_round ++);
             while (1);
         }
@@ -492,7 +586,7 @@ REPEAT:
     }
 }
 
-static void test_i2c_master_async_callback(int event)
+static void i2c_master_async_callback(int event)
 {
     callback_event = event;
 }
@@ -507,12 +601,12 @@ static void test_i2c_slave(void)
         int addr_status = i2c_slave.receive();
         switch (addr_status) {
             case I2CSlave::ReadAddressed:
-                i2c_slave.write(i2c_buf, sizeof (i2c_buf));
+                i2c_slave.write(i2c_test_ctx.buf, sizeof (i2c_test_ctx.buf));
                 break;
             case I2CSlave::WriteAddressed:
-                i2c_slave.read(i2c_buf, sizeof (i2c_buf));
-                for(int i = 0; i < sizeof (i2c_buf); i ++) {
-                    i2c_buf[i] ++;
+                i2c_slave.read(i2c_test_ctx.buf, sizeof (i2c_test_ctx.buf));
+                for(int i = 0; i < sizeof (i2c_test_ctx.buf); i ++) {
+                    i2c_test_ctx.buf[i] ++;
                 }
                 break;
         }
